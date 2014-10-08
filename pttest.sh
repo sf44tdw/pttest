261fb5b3e4d77a211fc8ce58bca59ba3344b1f7d
sf44tdw <prmkaddr@yahoo.co.jp>
2014-10-08 09:51:41 +0900

    1

---
+++ pttest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#受信できそうなチャンネルを片端から30秒間録画する。
+
+#カレントディレクトリ
+cdir=`dirname $0`
+#tsファイル保存先ディレクトリ
+tsdir=${cdir}/ts
+
+#前回のファイルが残っているかも知れないので、念のため削除
+rm -f ${tsdir}/*.ts
+
+for channel in`seq 1 300`
+do
+echo ${channel}
+/usr/local/bin/recpt1 --strip --b25 ${channel} 30 ${tsdir}/${channel}.ts
+done
