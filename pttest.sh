89feb3508653b8f6f04ed839c82c79b4dc2e08a0
sf44tdw <prmkaddr@yahoo.co.jp>
2016-08-17 09:19:08 +0900

    表示を変更。録画時間の指定を変数経由に変更。

--- pttest.sh
+++ pttest.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-#受信できそうなチャンネルを片端から30秒間録画する。
+#1から800までの値をrecpt1にチャンネルとして渡し、事前に指定した秒数だけ録画を試みる。
 
 #カレントディレクトリ
 cdir=`dirname $0`
+
 #tsファイル保存先ディレクトリ
 tsdir=${cdir}/ts
+
+#録画時間(秒)
+duration=90
 
 #前回のファイルが残っているかも知れないので、念のため削除
 rm -f ${tsdir}/*.ts
 
-for channel in {1..300}
+
+for channel in {1..800}
 do
-echo ${channel}
-/usr/local/bin/recpt1 --strip --b25 ${channel} 30 ${tsdir}/${channel}.ts
+echo ''
+echo "************************************************************************************************"
+echo time     = `date +%Y%m%d%H%M%S`
+echo  channel =  ${channel}
+/usr/local/bin/recpt1 --strip --b25 ${channel} ${duration} ${tsdir}/${channel}.ts
+echo "************************************************************************************************"
+echo ''
 done
